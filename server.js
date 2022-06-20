fc3a08ca1c5f2432f40d75bdcd88a5905fabacac
Norbiros <norbiros@protonmail.com>
2022-06-20 17:02:22 +0200

    First commit!

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child, get, set } from "firebase/database";
+
+const api = express();
+
+const firebaseConfig = {
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+  projectId: "radiobon-api",
+  storageBucket: "radiobon-api.appspot.com",
+  messagingSenderId: "638355162966",
+  appId: "1:638355162966:web:55a7165220993aa14378f7",
+  measurementId: "G-S5XNPKMYTZ",
+  databaseURL: `${process.env.FIREBASE_DATABASE_DOMAIN}`
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const PORT = process.env.PORT || 3000;
+api.listen(PORT);
+
+console.log('RadioBON API started on port 3000!');
+
+api.get("/broadcasts", async (req, res) => {
+  const dbRef = ref(getDatabase());
+  const data = await get(child(dbRef, `broadcasts/`));
+  if (data.exists()) {
+    res.json( data.val() );
+  } else {
+    res.send( "No data available!" );
+  }
+});
