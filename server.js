18155479e05680807eb23b7ea86e82e827a68862
Norbiros <norbiros@protonmail.com>
2022-07-19 19:10:55 +0200

    New table - ideas + bug fixes!

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import bodyParser from "body-parser";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, child, get, set } from "firebase/database";
 
@@ -25,11 +26,15 @@ api.listen(PORT);
 console.log("RadioBON API started on port " + PORT + "!");
 
 api.use(cors());
+api.use(bodyParser.urlencoded({ extended: false })) 
+api.use(bodyParser.json())
+
 
 api.get("/", async (req, res) => {
   res.send("RadioBON API!");
 });
 
+// Get API to show broadcasts
 api.get("/broadcasts", async (req, res) => {
   const dbRef = ref(getDatabase());
   const data = await get(child(dbRef, `broadcasts/`));
@@ -37,5 +42,52 @@ api.get("/broadcasts", async (req, res) => {
     res.json( data.val() );
   } else {
     res.send( "No data available!" );
+  }
+});
+
+// Post API to add new broadcasts
+api.post("/broadcast", async (req, res) => {
+  const dbRef = ref(getDatabase());
+  const data = await get(child(dbRef, `broadcasts/`));
+  let index  = data.val()?.length;
+  if (!!req.body.title && !!req.body.description) {
+    index = index ? index : 0;
+    set(ref(database, ('broadcasts/' + index)), {
+      Title: req.body.title,
+      Description: req.body.description
+    });
+    res.status(200).send({message: "Succesfully added data to database!"});
+  } else {
+    res.status(500).send({message: "You didn't provide broadcasts title/description OR there is database error!"});
+  }
+});
+
+// Get API to show broadcasts
+api.get("/broadcasts", async (req, res) => {
+  const dbRef = ref(getDatabase());
+  const data = await get(child(dbRef, `ideas/`));
+  if (data.exists()) {
+    res.json( data.val() );
+  } else {
+    res.send( "No data available!" );
+  }
+});
+
+// Post API to add new broadcasts
+api.post("/new-idea", async (req, res) => {
+  const dbRef = ref(getDatabase());
+  const data = await get(child(dbRef, `ideas/`));
+  let index  = data.val()?.length;
+  if (!!req.body.title && !!req.body.description  && !!req.body.category  && !!req.body.date) {
+    index = index ? index : 0;
+    set(ref(database, ('ideas/' + index)), {
+      Title: req.body.title,
+      Description: req.body.description,
+      Category: req.body.category,
+      Date: req.body.date
+    });
+    res.status(200).send({message: "Succesfully added data to database!"});
+  } else {
+    res.status(500).send({message: "You didn't provide broadcasts title/description/category/date there is database error!"});
   }
 });
